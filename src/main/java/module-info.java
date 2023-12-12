92198467f9abcf1889205f677ac139886539ba68
watanabe mikio <bp21062@sibaura-it.ac.jp>
2023-12-13 01:52:30 +0900

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.hl_appserver {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.hl_appserver to javafx.fxml;
+    exports com.example.hl_appserver;
+}
