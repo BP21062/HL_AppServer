8b761a2c523237c055f400848e4b5b272fef0009
bp21012koga <bp21012@shibaura-it.ac.jp>
2023-12-20 14:41:50 +0900

    AControllerクラスを追加

---
+++ src/main/java/com/example/hl_appserver/AController.java
@@ -0,0 +1,21 @@
+package com.example.hl_appserver;
+
+public class AController{
+
+	public void displayCurrentPoint(){}
+
+	public void choiceDeckAndCardList(){}
+
+	public void displayChoice(){}
+
+	public void displaySecondCard(){}
+
+	public void changeResultScreen(){}
+
+	public void recordResult(){}
+
+	public void checkRoomState(){}
+
+
+
+}
