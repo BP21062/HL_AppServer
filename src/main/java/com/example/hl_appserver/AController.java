514a9c6be8bad973d7185150d89a3b7db77a889e
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-22 23:00:54 +0900

    初期ステータス

--- src/main/java/com/example/hl_appserver/AController.java
+++ src/main/java/com/example/hl_appserver/AController.java
@@ -12,10 +12,21 @@ public class AController{
 
 	public void changeResultScreen(){}
 
-	public void recordResult(){}
+	public void recordResult(){
+	}
 
-	public void checkRoomState(){}
+	public void checkRoomState(){
+	}
 
+	public static void main(String[] args){
+		GameController game1 = new GameController(1);
+		GameController game2 = new GameController(2);
+		GameController game3 = new GameController(3);
+		GameController game4 = new GameController(4);
+		GameController game5 = new GameController(5);
+		GameController game6 = new GameController(6);
+
+	}
 
 
 }
--- src/main/java/com/example/hl_appserver/GameController.java
+++ src/main/java/com/example/hl_appserver/GameController.java
@@ -1,20 +1,30 @@
 package com.example.hl_appserver;
 
-import java.util.ArrayList;
 
 public class GameController{
-	ArrayList<Room> room_list = new ArrayList<>();
+	//ArrayList<Room> room_list = new ArrayList<>();
+	public Room room;
 
-	public void startGame(){}
+	public void startGame(){
+	}
 
-	public void displayFirstCard(){}
+	public void displayFirstCard(){
+	}
 
-	public void enterRoom(){}
+	public void enterRoom(){
+	}
 
-	public void decideRoom(){}
+	public void decideRoom(){
+	}
 
-	public void checkConnectionState(){}
+	public void checkConnectionState(){
+	}
 
-	public void setTimer(){}
+	public void setTimer(){
+	}
+
+	public GameController(int room_id){
+		this.room = new Room(room_id);
+	}
 
 }
