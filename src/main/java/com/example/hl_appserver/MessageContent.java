06dc8e0a66ff2bbcf725f29a4273c9c53225a6f1
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-26 23:45:39 +0900

    データ送信用のメッセージコンテントの修正

--- src/main/java/com/example/hl_appserver/MessageContent.java
+++ src/main/java/com/example/hl_appserver/MessageContent.java
@@ -1,11 +1,19 @@
 package com.example.hl_appserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageContent{
 
 	String user_id;
 	String password;
-	String score;
-	String rule;
+	//String score;
+	int num_plays_score;
+	int num_hits_score;
+	int num_wins_score;
+	//String rule;
+	//カードおよびルールの画像ファイル(base64)格納用のリスト
+	List<String> textDataList = new ArrayList<>();
 	String error;
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.hl_appserver {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javax.websocket.api;
+	requires com.google.gson;
+	requires tyrus.server;
 
 
 	opens com.example.hl_appserver to javafx.fxml;
