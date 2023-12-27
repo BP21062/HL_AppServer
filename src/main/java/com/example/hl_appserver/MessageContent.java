93e10c7f7e25802c097744a98c4c68b3e20ed105
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-27 22:17:07 +0900

    メッセージクラスの再調整

--- src/main/java/com/example/hl_appserver/MessageContent.java
+++ src/main/java/com/example/hl_appserver/MessageContent.java
@@ -7,7 +7,7 @@ public class MessageContent{
 
 	String user_id;//user_id格納用
 	String password;//password格納用
-	String room_id;//入りたい部屋もしくは入ってる部屋
+	int room_id;//入りたい部屋もしくは入ってる部屋
 	String choice;//選んだ選択肢を代入
 	String pattern;//選んだ絵柄を代入
 	int num_plays_score;//戦績 play回数
