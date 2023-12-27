f6fe97398be97d05e116da92549e917b33d7c9a4
bp21009 <bp21009@shibaura-it.ac.jp>
2023-12-27 22:14:36 +0900

    メッセージクラスの調整

--- src/main/java/com/example/hl_appserver/MessageContent.java
+++ src/main/java/com/example/hl_appserver/MessageContent.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class MessageContent{
 
-	String user_id;
-	String password;
-	//String score;
-	int num_plays_score;
-	int num_hits_score;
-	int num_wins_score;
-	//String rule;
-	//カードおよびルールの画像ファイル(base64)格納用のリスト
-	List<String> textDataList = new ArrayList<>();
+	String user_id;//user_id格納用
+	String password;//password格納用
+	String room_id;//入りたい部屋もしくは入ってる部屋
+	String choice;//選んだ選択肢を代入
+	String pattern;//選んだ絵柄を代入
+	int num_plays_score;//戦績 play回数
+	int num_hits_score;//戦績　hit回数
+	int num_wins_score;//戦績　勝利数
+	List<String> textDataList = new ArrayList<>();//カードおよびルールの画像ファイル(base64)格納用のリスト
 	String error;
 
 }
