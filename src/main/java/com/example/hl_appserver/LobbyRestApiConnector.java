ecd37f27e038e412e6451b7f74c01cc01d8f2188
bp21009 <bp21009@shibaura-it.ac.jp>
2024-01-05 02:39:46 +0900

    コードの修正

--- src/main/java/com/example/hl_appserver/LobbyRestApiConnector.java
+++ src/main/java/com/example/hl_appserver/LobbyRestApiConnector.java
@@ -26,21 +26,16 @@ public class LobbyRestApiConnector{
 	 * ルームに入れるかどうかをJsonで返す
 	 * trueの場合には、AServerConnectorにuser_idを記憶する。
 	 * @param request 受け取ったJson(room_idとuser_idが埋まっているもの)
-	 * @return 真理値の詰まったJson　部屋に入れるならtrue 入れなければfalse
-	 */
-	public Response checkRoomState(String request){
+	 * @return 真理値　部屋に入れるならtrue 入れなければfalse
+	 */ public Response checkRoomState(String request){
 		//メッセージを解凍
 		Message request_message = gson.fromJson(request, Message.class);
 		//checkRoomStateがtrueならuser_idを記憶
 		if(aController.checkRoomState(request_message.messageContent.room_id)){
 			aController.memorizeUser(request_message.messageContent.user_id);
 		}
-		//返信用のメッセージを作成　通信規則はいらないけど、コンストラクタで指定してしまっているため...(一応シーケンス図に記載の番号を詰めました)
-		Message send_message = new Message("6000", request_message.messageContent.user_id);
-		//部屋の入室可否をメッセージに保存
-		send_message.result = aController.checkRoomState(request_message.messageContent.room_id);
-		//レスポンスを返す(true falseに関わらずhttpレスポンスが200で固定になってしまうかも？？？)
-		return Response.ok().entity(gson.toJson(send_message)).build();
+		//よくよく考えればbooleanだけ返せばよくね...？ part.1
+		return Response.ok().entity(gson.toJson(aController.checkRoomState(request_message.messageContent.room_id))).build();
 	}
 
 	@Path("/checkRoomCount")
@@ -49,17 +44,10 @@ public class LobbyRestApiConnector{
 	/**
 	 * checkRoomCountメソッド
 	 * ルームの在室人数をJsonで返す MessageContentクラスのroom_user_countに部屋１～６の順で返す
-	 * @param request 受け取ったJson(room_idとuser_idが埋まっているもの)
-	 * @return Json(room_user_countが埋まっているもの)
-	 */
-	public Response checkRoomCount(String request){
-		//メッセージの解凍
-		Message request_message = gson.fromJson(request, Message.class);
-		//送信メッセージの作成()
-		Message send_message = new Message("6001", request_message.messageContent.user_id);
-		//部屋の人数のカウントを格納
-		send_message.messageContent.room_user_count = aController.checkRoomCount();
-		//メッセージを送信
-		return Response.ok().entity(gson.toJson(send_message)).build();
+	 * @return List<Integer> 部屋部屋１～６の人数
+	 */ public Response checkRoomCount(){
+		//よくよく考えればListだけ返せばよくね...？ part.2
+		return Response.ok().entity(gson.toJson(aController.checkRoomCount())).build();
+
 	}
 }
