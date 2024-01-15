a85967bec536ffd5e19fd5629fbde8b093eb69ee
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2024-01-15 23:22:43 +0900

    DBの不必要なメソッド削除

--- src/main/java/com/example/hl_appserver/DatabaseConnector.java
+++ src/main/java/com/example/hl_appserver/DatabaseConnector.java
@@ -24,93 +24,13 @@ public class DatabaseConnector{
 		}
 	}
 
-	public static List<Integer> getScore(String user_id){
-		List<Integer> scoreDataList = null;
-		try{
-
-			// 接続先はこんな感じの文字列->jdbc:mysql://sql.yamazaki.se.shibaura-it.ac.jp:13307/データベース名
-			String target = url + ":" + sqlServerPort + "/" + sqlDatabaseName;
-			System.out.println("target: " + target);
-
-			// 接続先情報と"MySQLへログインするための"ユーザIDとパスワードから接続を行う
-			Connection connection = DriverManager.getConnection(target, sqlUserId, sqlPassword);
-			// MySQLに問い合わせるためのStatementオブジェクトを構築する
-			Statement stmt = connection.createStatement();
-			// 実際にMySQLデータベースサーバに問い合わせるときのクエリメッセージを作る
-			// ここはやりたい処理によって大きく変わることに注意
-			String queryString = "SELECT * FROM SCORE WHERE user_id = '" + user_id + "';";
-
-			// Statementオブジェクトとクエリメッセージを使い，実際に問い合わせて結果を得る
-			ResultSet rs = stmt.executeQuery(queryString);
-
-			// 得られた結果の集合から必要なデータを取り出す
-			scoreDataList = new ArrayList<>();
-			// 0→plays 1→hits 2→wins
-
-
-			while(rs.next()){
-				System.out.println(rs.getString(1));
-				scoreDataList.add(Integer.parseInt(rs.getString(2)));
-				scoreDataList.add(Integer.parseInt(rs.getString(3)));
-				scoreDataList.add(Integer.parseInt(rs.getString(4)));
-				//textDataList.add(rs.getString(1));
-			}
-			System.out.println("List Elements:");
-			for(int element : scoreDataList){
-				System.out.println(element);
-			}
-
-
-		}catch(SQLException e){
-			e.printStackTrace();
-		}finally{
-
-		}
-		return scoreDataList;
-	}
-
-	public static String getRule(){
-		String rule = null;
-		try{
-
-			// 接続先はこんな感じの文字列->jdbc:mysql://sql.yamazaki.se.shibaura-it.ac.jp:13307/データベース名
-			String target = url + ":" + sqlServerPort + "/" + sqlDatabaseName;
-			System.out.println("target: " + target);
-
-			// 接続先情報と"MySQLへログインするための"ユーザIDとパスワードから接続を行う
-			Connection connection = DriverManager.getConnection(target, sqlUserId, sqlPassword);
-			// MySQLに問い合わせるためのStatementオブジェクトを構築する
-			Statement stmt = connection.createStatement();
-			// 実際にMySQLデータベースサーバに問い合わせるときのクエリメッセージを作る
-			// ここはやりたい処理によって大きく変わることに注意
-			String queryString = "SELECT rule FROM RULE;";
-
-			// Statementオブジェクトとクエリメッセージを使い，実際に問い合わせて結果を得る
-			ResultSet rs = stmt.executeQuery(queryString);
-
-			// 得られた結果の集合から必要なデータを取り出す
-
-
-			while(rs.next()){
-				System.out.println(rs.getString(1));
-				rule = rs.getString(1);
-			}
-
-
-		}catch(SQLException e){
-			e.printStackTrace();
-		}
-		return rule;
-
-	}
-
 	public static void saveScore(String user_id, int num_hit, boolean num_wins){
 		int win = 0;
 		try{
 
 			// 接続先はこんな感じの文字列->jdbc:mysql://sql.yamazaki.se.shibaura-it.ac.jp:13307/データベース名
 			String target = url + ":" + sqlServerPort + "/" + sqlDatabaseName;
-			System.out.println("target: " + target);
+			System.out.println("[App] saveScore: Access to " + target);
 
 			// 接続先情報と"MySQLへログインするための"ユーザIDとパスワードから接続を行う
 			Connection connection = DriverManager.getConnection(target, sqlUserId, sqlPassword);
@@ -146,7 +66,7 @@ public class DatabaseConnector{
 
 				// 接続先はこんな感じの文字列->jdbc:mysql://sql.yamazaki.se.shibaura-it.ac.jp:13307/データベース名
 				String target = url + ":" + sqlServerPort + "/" + sqlDatabaseName;
-				System.out.println("target: " + target);
+				System.out.println("[App] getCardList: Access to " + target);
 
 				// 接続先情報と"MySQLへログインするための"ユーザIDとパスワードから接続を行う
 				Connection connection = DriverManager.getConnection(target, sqlUserId, sqlPassword);
